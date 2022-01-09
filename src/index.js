7af8e4e0cd14a9344bc0520176dbf61580738a13
SergeyZholobov <zholobov@mail.ru>
2022-01-09 17:28:10 +0300

    task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let result = '';
+    let number = Math.abs(n);
+    let str = ''+number;
+    let i = 0;
+    while (i <= str.length-1) {
+        result = str[i] + result;
+        i = i+1;
+    }
+    return Number(result);
 }
